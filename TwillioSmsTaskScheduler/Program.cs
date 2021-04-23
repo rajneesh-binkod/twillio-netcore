1ec439bd7b3600cae091b27b80ec962be13a998c
developer <developer.binkod@gmail.com>
2021-04-23 14:21:05 +0530

    Task Scheduler

---
+++ TwillioSmsTaskScheduler/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace TwillioSmsTaskScheduler
+{
+    class Program
+    {
+        private static readonly string FROM = "PHONE NUMBER HERE";
+        private static readonly string AccountSID = "ACCOUNT SID HERE";
+        private static readonly string AuthTOKEN = "AUTH TOKEN HERE";
+
+        static void Main(string[] args)
+        {
+            TwilioClient.Init(AccountSID, AuthTOKEN);
+
+            string MessageText = "Hi Rajneesh, sending a test SMS on " + DateTime.Now + " for task scheduler demo from Twillio.";
+            string MessageTO = "PHONE NUMBER HERE";
+
+            MessageResource.Create(
+                body: MessageText,
+                from: new Twilio.Types.PhoneNumber(FROM),
+                to: new Twilio.Types.PhoneNumber(MessageTO)
+            );
+        }
+    }
+}
